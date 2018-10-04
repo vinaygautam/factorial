e0a6bd19226fbb84c8472a79022108ae4697102f
pulkitgupta2k <pulkitgupta2k@gmail.com>
2018-10-04 16:37:30 +0530

    made code readable

--- factorial.c
+++ factorial.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 int main()
-{int n,i;
-scanf("%d",&n);
-int prod=1;
-for(i=n;i>=1;i--)
-{prod=prod*i;
-}
-printf("factorial %d",prod);
+{
+    int n,i;
+    scanf("%d",&n);
+    int prod=1;
+    for(i=2;i<n;i++)
+        prod=prod*i;
+    printf("factorial %d",prod);
 }
