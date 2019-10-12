1cdc62883e73fb44a58e70ef0e55b9369d90adf8
vinaygautam <56476804+vinaygautam@users.noreply.github.com>
2019-10-12 17:48:10 +0530

    Update factorial.c

--- factorial.c
+++ factorial.c
@@ -8,3 +8,26 @@ int main()
     for(i=2;i<n;i++)
         prod=prod*i;
     printf("factorial %d",prod);
+    
+}
+python
+# Python program to find the factorial of a number provided by the user.
+
+# change the value for a different result
+num = 7
+
+# uncomment to take input from the user
+#num = int(input("Enter a number: "))
+
+factorial = 1
+
+# check if the number is negative, positive or zero
+if num < 0:
+   print("Sorry, factorial does not exist for negative numbers")
+elif num == 0:
+   print("The factorial of 0 is 1")
+else:
+   for i in range(1,num + 1):
+       factorial = factorial*i
+   print("The factorial of",num,"is",factorial)
+           
